8869bea515f91f11ef21baa514f6938363c968c8
Ehor <123355005+Ehor-developer@users.noreply.github.com>
2023-03-27 17:00:59 +0900

    修正を行いました

--- saizencode.ts
+++ saizencode.ts
@@ -29,7 +29,7 @@ router
         <meta property="og:site_name" content="Nuller-Apps" />
         <meta property="og:description" content="このアプリは、サイゼリヤのメニュー番号を手軽に検索できる便利なツールです。メニュー番号からは、直接その商品を注文することができます。また、メニュー名からメニュー番号に変換することもできます。ただし、このツールは非公式のものであり、実際の情報と異なる場合があることに注意してください。" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://saizencode.deno.dev/ocg/Saizencode.png" />
+        <meta name="twitter:image" content="https://raw.githubusercontent.com/Nuller-Team/saizencode/main/ocg/Saizencode.png" />
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="600">
     </head>
