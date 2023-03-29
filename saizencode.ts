e6ba8287bc945cb1e1630ab5b2f1e50305746a1c
Ehor <123355005+Ehor-developer@users.noreply.github.com>
2023-03-29 15:04:50 +0900

    デザインの修正を行いました。

--- saizencode.ts
+++ saizencode.ts
@@ -33,19 +33,17 @@ router
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="600">
     </head>
-
-    <body class="bg-gray-100 min-h-screen flex items-center justify-center p-3">
-
-        <div class="bg-white py-40 px-20 rounded-lg shadow-lg w-100">
-            <div class="bg-yellow-100 my-3 border-l-4 border-yellow-400 p-4" role="alert">
-                <p class="font-bold">注意事項</p>
-                <p>このアプリは非公式のものです。そのため、情報や価格については正確でない可能性があります。あらかじめご了承ください。</p>
-            </div>
-            <h1 class="text-5xl font-bold mb-4">Saizencode🥗</h1>
-            <p class="text-2xl font-bold mb-4">サイゼリヤのメニュー名やメニュー番号でメニュー詳細を検索することができます。</p>
-            <form method="get" action="/search" class="flex">
-                <input type="text" name="query"
-                    class="rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white p-2 w-full"
+    <body class="bg-gray-100 min-h-screen flex items-center justify-center px-2 lg:px-0">
+      <div class="bg-white rounded-lg shadow-lg w-full lg:max-w-screen-md mx-auto">
+        <div class="bg-yellow-100 my-3 border-l-4 border-yellow-400 p-4">
+          <p class="font-bold">⚠️ 注意事項</p>
+          <p>このアプリは非公式のものです。そのため、情報や価格については正確でない可能性があります。あらかじめご了承ください。</p>
+        </div>
+        <h1 class="text-4xl lg:text-5xl font-bold text-center mb-4">Saizencode🥗</h1>
+        <p class="text-xl lg:text-2xl font-bold text-center mb-8">サイゼリヤのメニュー名やメニュー番号でメニュー詳細を検索することができます。</p>
+        <form method="get" action="/search" class="flex">
+          <input type="text" name="query"
+            class="rounded-l-lg border-t border-b border-l text-gray-800 border-gray-200 bg-white p-2 w-full lg:w-3/4 xl:w-2/3"
                     placeholder="検索例）SA02" style="font-size: 16pt;" />
                 <button type="submit"
                     class="px-4 bg-blue-500 text-white font-semibold rounded-r-lg border-t border-b border-r hover:bg-blue-700">
@@ -121,19 +119,18 @@ router
         <meta name="twitter:image" content="https://raw.githubusercontent.com/Nuller-Team/saizencode/main/ocg/Saizencode.png" />
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="600">
-
-    <body class="bg-gray-100 min-h-screen flex items-center justify-center p-3">
-
-        <div class="bg-white py-40 px-20 rounded-lg shadow-lg w-100">
-            <div class="bg-yellow-100 my-3 border-l-4 border-yellow-400 p-4" role="alert">
-                <p class="font-bold">注意事項</p>
-                <p>このアプリは非公式のものです。そのため、情報や価格については正確でない可能性があります。あらかじめご了承ください。</p>
-            </div>
-            <h1 class="text-5xl font-bold mb-4">Saizencode🥗</h1>
-            <p class="text-2xl font-bold mb-4">サイゼリヤのメニュー名やメニュー番号でメニュー詳細を検索することができます。</p>
-            <form method="get" action="/search" class="flex">
-                <input type="text" name="query"
-                    class="rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white p-2 w-full"
+    </head>
+    <body class="bg-gray-100 min-h-screen flex items-center justify-center px-2 lg:px-0">
+      <div class="bg-white rounded-lg shadow-lg w-full lg:max-w-screen-md mx-auto">
+        <div class="bg-yellow-100 my-3 border-l-4 border-yellow-400 p-4">
+          <p class="font-bold">⚠️ 注意事項</p>
+          <p>このアプリは非公式のものです。そのため、情報や価格については正確でない可能性があります。あらかじめご了承ください。</p>
+        </div>
+        <h1 class="text-4xl lg:text-5xl font-bold text-center mb-4">Saizencode🥗</h1>
+        <p class="text-xl lg:text-2xl font-bold text-center mb-8">サイゼリヤのメニュー名やメニュー番号でメニュー詳細を検索することができます。</p>
+        <form method="get" action="/search" class="flex">
+          <input type="text" name="query"
+            class="rounded-l-lg border-t border-b border-l text-gray-800 border-gray-200 bg-white p-2 w-full lg:w-3/4 xl:w-2/3"
                     placeholder="検索例）SA02" style="font-size: 16pt;" />
                 <button type="submit"
                     class="px-4 bg-blue-500 text-white font-semibold rounded-r-lg border-t border-b border-r hover:bg-blue-700">
